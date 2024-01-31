e8636b263a88215baa04987d02a04288fbfa5e5d
Paramvir singh <114574158+paramvirTheBlockCoders@users.noreply.github.com>
2024-01-31 15:37:30 +0530

    Add files via upload

---
+++ pages/api/getgamezop1.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+// import http from 'http';
+
+export default async function handler(req, res) {
+  // const session = await getSession({req})
+  if (req.method === "POST") {
+    try {
+      const {token} = req.body;
+      const {data1} = req.body;
+      console.log(data1,"data 1");
+      const data = { buttonValue: data1.buttonValue };
+console.log(data, "data");
+      // const agent = new http.Agent({
+      //   rejectUnauthorized: false,
+      // });
+      var config = {
+        method: "post",
+        url: "http://airaicloud.com:6000/api/v1/admin/getlinks",
+        headers: {
+          'Authorization': `Bearer ${token}`
+        },data,
+      };
+      await axios(config).then(function (response) {
+        console.log(JSON.stringify(response.data));
+        res.status(200).json({ data: response.data });
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ Error: err });
+    }
+  }
+}
+
+
+
